41d204d4cfa84ace29320e384714d2f41e8ffe20
student14 <student14@apples-macbook-air-4.local>
2016-12-26 12:48:02 +0530

    Initial Commit

---
+++ StopWatch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StopWatch
+//
+//  Created by student14 on 26/12/16.
+//  Copyright © 2016 Felix-ITs-Divesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StopWatch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StopWatch
+//
+//  Created by student14 on 26/12/16.
+//  Copyright © 2016 Felix-ITs-Divesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
